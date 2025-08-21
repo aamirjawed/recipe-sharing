cf4883142f6e3fe4caeccc0a431a0ce0e764b055
aamirjawed <aamirjawed682@gmail.com>
2025-08-21 14:25:50 +0530

    send user data to frontend

--- src/routes/meRoutes.js
+++ src/routes/meRoutes.js
@@ -1,20 +1,30 @@
 import express from 'express'
 import authUser from '../middleware/authMiddleware.js'
+import User from '../models/userModel.js'
 
 const router = express.Router()
 
 
-router.get('/me', authUser, (req, res) => {
+router.get('/me', authUser, async (req, res) => {
     try {
+
+        const user = await User.findByPk(req.userId,{
+            attributes:{exclude :['password']}
+        })
+
         res.status(200).json({
         id:req.userId, 
-        user:req.role})
+        user:req.role,
+        data:user
+    })
+
+        
     } catch (error) {
         console.log("Error in me routes", error.message)
         console.log("Full error", error)
         res.status(500).json({
             message:"Failed to fetch user"
-        })
+        }) 
     }
 })
 
