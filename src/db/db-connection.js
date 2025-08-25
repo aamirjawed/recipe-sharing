0649c8dc5471255558b7a601dbd0e40cbdb696ee
aamirjawed <aamirjawed682@gmail.com>
2025-08-25 09:17:12 +0530

    db changes

--- src/db/db-connection.js
+++ src/db/db-connection.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: 4000, 
+    port: process.env.DB_PORT || 3306, 
     dialect: 'mysql',
     dialectOptions: {
       ssl: {
