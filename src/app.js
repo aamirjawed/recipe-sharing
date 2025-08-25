73f8f5648bd232f9aa1ff2a03ef1b313b8889356
aamirjawed <aamirjawed682@gmail.com>
2025-08-25 09:34:47 +0530

    cors update in app.js

--- src/app.js
+++ src/app.js
@@ -30,11 +30,15 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+// handle preflight requests
+app.options('*', cors());
+
 
 app.use(express.json({
     limit:"16kb"
